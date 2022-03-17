a25c18a0b6c6d9c8cc52637f12747ab88a895dfb
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-03-17 20:00:28 +1100

    prepare deploy

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,6 +7,7 @@ import {errorHandler} from './middlewares/errorHandler';
 import mongoose from "mongoose";
 import {userRouter} from "./routes/userRouter";
 import cors from 'cors'
+import path from 'path';
 
 // app will recognize the process.env.xxx variables
 dotenv.config();
@@ -30,11 +31,22 @@ mongoose
 		app.use('/api/tasks', taskRouter);
 		app.use('/api/users', userRouter);
 
+		// serve client
+		if (process.env.NODE_ENV === 'production') {
+			app.use(express.static(path.join(__dirname, '..', '..', 'client/build')));
+
+			app.get('*', (req: Request, res: Response) => res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html')));
+
+		} else {
+			app.get('/', (req: Request, res: Response) => res.send('Please set NODE_ENV=production'))
+		}
+
 		app.use(errorHandler);
 
 		app.listen(PORT, () => {
 			console.log(`the server is running on port of ${PORT}`);
-		})
+		});
+
 	}
 )
 	.catch(err => {
